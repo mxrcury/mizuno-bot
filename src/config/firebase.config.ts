4e50e99e23e5a99a1e548e4ce9a8375acf7ff14f
mxrcury <demetrgonchar@gmail.com>
2023-05-27 14:41:15 +0300

    deleted firebase config with creds

--- src/config/firebase.config.ts
+++
@@ -1,6 +0,0 @@
-import { ServiceAccount } from 'firebase-admin';
-
-export const firebaseOptions: ServiceAccount = {
-  //   private_key_id: '3fa575ec5d4f054d1c0085bf6a67f44e521f8c7c',
-  //   client_id: '116421580578251612631',
-};
