976a547eaf096b438c264c13a48f3bcc2b539114
mxrcury <demetrgonchar@gmail.com>
2023-05-29 06:15:46 +0300

    updated downloading audio

--- src/modules/bot/bot.update.ts
+++ src/modules/bot/bot.update.ts
@@ -1,8 +1,8 @@
 import { Command, Ctx, Start, Update } from 'nestjs-telegraf';
 
 import { HttpService } from '@nestjs/axios';
-import { resolve } from 'path';
 import { IBotContext } from '../../interfaces/bot.interface';
+import { YoutubeService } from '../youtube/youtube.service';
 import { BotService } from './bot.service';
 
 @Update()
@@ -10,6 +10,7 @@ export class BotUpdate {
   constructor(
     private readonly botService: BotService,
     private readonly httpService: HttpService,
+    private readonly youtubeService: YoutubeService,
   ) {}
 
   @Start()
@@ -19,23 +20,33 @@ export class BotUpdate {
     );
   }
 
-  @Command('extractAudio')
-  async extractAudio(@Ctx() ctx: IBotContext) {
-    ctx.reply('Welcome! Please enter your name:');
-    const parsedArgs = this.botService.parseCommandArgs(
-      ctx.update.message.text,
-    );
+  @Command('download')
+  async download(@Ctx() ctx: IBotContext) {
+    console.time('Downloading-video-audio');
+    const link = ctx.update.message.text.split(' ')[1];
+    const video = (await this.youtubeService.getAudio(link)) as Buffer;
 
-    await this.botService.extractAudioFromVideo(
-      resolve(...parsedArgs.video.split('/')),
-      resolve(...parsedArgs.audio.split('/')),
-    );
-    await this.botService.mergeAudioWithVideo(
-      resolve(...parsedArgs.videoSub.split('/')),
-      resolve(...parsedArgs.audio.split('/')),
-      resolve(...parsedArgs.outputVideo.split('/')),
-    );
-
-    await ctx.reply('Finished🔥');
+    console.timeEnd('Downloading-video-audio');
+    await ctx.replyWithAudio({ source: video, filename: 'some-file.mp3' });
   }
+
+  // @Command('extractAudio')
+  // async extractAudio(@Ctx() ctx: IBotContext) {
+  //   ctx.reply('Welcome! Please enter your name:');
+  //   const parsedArgs = this.botService.parseCommandArgs(
+  //     ctx.update.message.text,
+  //   );
+
+  //   await this.botService.extractAudioFromVideo(
+  //     resolve(...parsedArgs.video.split('/')),
+  //     resolve(...parsedArgs.audio.split('/')),
+  //   );
+  //   await this.botService.mergeAudioWithVideo(
+  //     resolve(...parsedArgs.videoSub.split('/')),
+  //     resolve(...parsedArgs.audio.split('/')),
+  //     resolve(...parsedArgs.outputVideo.split('/')),
+  //   );
+
+  //   await ctx.reply('Finished🔥');
+  // }
 }
--- src/modules/youtube/youtube.service.ts
+++ src/modules/youtube/youtube.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { createWriteStream } from 'fs';
+import { spawn } from 'child_process';
 const ytdl = require('ytdl-core');
 
 const request = require('youtube-dl-exec');
@@ -9,67 +9,110 @@ const yt = require('ytmp3-scrap');
 
 @Injectable()
 export class YoutubeService {
-  downloader;
   constructor(private readonly httpService: HttpService) {}
-  // onModuleInit() {
-  // throw new Error('Method not implemented.');
-  // }
-  // onModuleInit() {}
 
   async getAudio(link: string) {
-    const res = await ytdl(link, { filter: 'audioonly' });
-    res.pipe(createWriteStream('audio.mp3'));
-    // console.log(res.pipe());
-    // const info = await ytdl.getInfo(link);
-    // const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    // console.log('--', audioFormats);
+    return new Promise(async (resolve, reject) => {
+      const args = [
+        link,
+        '-x',
+        '--audio-format',
+        'mp3',
+        '--audio-quality',
+        '192K',
+        '--audio-duration',
+        '999999',
+        '-o',
+        '-',
+        '--no-cache-dir',
+      ];
+      let i = 1;
+      const res = spawn('yt-dlp', args);
+      let stdout = Buffer.alloc(0);
+      let stderr = Buffer.alloc(0);
+
+      res.stdout.on('data', (data) => {
+        console.log(i);
+        i++;
+        stdout = Buffer.concat([stdout, data]);
+      });
+
+      res.stderr.on('data', (data) => {
+        stderr = Buffer.concat([stderr, data]);
+      });
+
+      res.on('close', (code) => {
+        if (code === 0) {
+          console.log('output');
+
+          resolve(stdout);
+        } else {
+          const errorMessage = stderr.toString('utf8');
+          reject(new Error(`Error extracting audio: ${errorMessage}`));
+        }
+      });
+      // res.on('close', (code) => {
+      //   if (code === 0) {
+      //     resolve(stdout);
+      //   } else {
+      //     const errorMessage = stderr.toString('utf8');
+      //     reject(new Error(`Error extracting audio: ${errorMessage}`));
+      //   }
+      // });
+    });
+
+    // s.stdout.on('error', (err) => {
+    //   reject('Error');
+    // });
+    // s.stderr.on('error', (err) => {
+    //   reject('err 2');
+    // });
+    // async $getAudio(link: string) {
+    //   try {
+    //     const response = await request(link, {
+    //       dumpSingleJson: true,
+    //       noCheckCertificates: true,
+    //       noWarnings: true,
+    //       f: 'bestaudio[ext=m4a]/m4a',
+    //       addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+    //       playlistItems: '1',
+    //       externalDownloader: 'aria2c',
+    //     });
+
+    //     // const file = response.requested_formats.find((r) => r.audio_ext === 'webm');
+    //     // const filename = response.title + `.${file.ext}`;
+
+    //     // const path = resolve(filename);
+    //     // const ws = await createWriteStream(path);
+
+    //     // const re = await fetch(file.url);
+    //     // re.body.pipe(ws);
+    //     // await writeFile(filename)
+    //     await writeFile('response.json', JSON.stringify(response));
+    //     const url = response.requested_downloads[0].url;
+    //     const filename =
+    //       response.title + `.${response.requested_downloads[0].ext}`;
+
+    //     console.log(url, filename);
+
+    //     // const filename = response.title + `.${file.ext}`;
+
+    //     const path = resolve(filename);
+
+    //     console.log(path);
+
+    //     const ws = await createWriteStream(path);
+    //     const re = await get(url);
+    //     // console.log(re);
+    //     console.log(re);
+
+    //     re.pipe(ws);
+
+    //     return { url, filename };
+    //   } catch (error) {
+    //     console.log('ERR - ', error);
+    //     return { url: '', filename: '' };
+    //   }
+    // }
   }
-
-  // async $getAudio(link: string) {
-  //   try {
-  //     const response = await request(link, {
-  //       dumpSingleJson: true,
-  //       noCheckCertificates: true,
-  //       noWarnings: true,
-  //       f: 'bestaudio[ext=m4a]/m4a',
-  //       addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
-  //       playlistItems: '1',
-  //       externalDownloader: 'aria2c',
-  //     });
-
-  //     // const file = response.requested_formats.find((r) => r.audio_ext === 'webm');
-  //     // const filename = response.title + `.${file.ext}`;
-
-  //     // const path = resolve(filename);
-  //     // const ws = await createWriteStream(path);
-
-  //     // const re = await fetch(file.url);
-  //     // re.body.pipe(ws);
-  //     // await writeFile(filename)
-  //     await writeFile('response.json', JSON.stringify(response));
-  //     const url = response.requested_downloads[0].url;
-  //     const filename =
-  //       response.title + `.${response.requested_downloads[0].ext}`;
-
-  //     console.log(url, filename);
-
-  //     // const filename = response.title + `.${file.ext}`;
-
-  //     const path = resolve(filename);
-
-  //     console.log(path);
-
-  //     const ws = await createWriteStream(path);
-  //     const re = await get(url);
-  //     // console.log(re);
-  //     console.log(re);
-
-  //     re.pipe(ws);
-
-  //     return { url, filename };
-  //   } catch (error) {
-  //     console.log('ERR - ', error);
-  //     return { url: '', filename: '' };
-  //   }
-  // }
 }
